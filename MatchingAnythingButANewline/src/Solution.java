71d4a4cf53217386f6d75b14b214af1ec53d0cfa
SVFBr <sheilafreire.br@gmail.com>
2016-09-13 15:57:42 -0300

    Matching Anything But a Newline

---
+++ MatchingAnythingButANewline/src/Solution.java
@@ -0,0 +1,9 @@
+// https://www.hackerrank.com/challenges/matching-anything-but-new-line/submissions/code/27535038
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        Tester tester = new Tester();
+		 tester.check("...\\....\\....\\....");
+    }
+    }
