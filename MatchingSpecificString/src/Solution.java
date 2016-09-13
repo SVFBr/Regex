13086f711b398dfc91f5d71056ed4213e2c6218f
SVFBr <sheilafreire.br@gmail.com>
2016-09-13 15:41:20 -0300

    Matching Specific String

---
+++ MatchingSpecificString/src/Solution.java
@@ -0,0 +1,11 @@
+//https://www.hackerrank.com/challenges/matching-specific-string/submissions/code/27534181
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		Regex_Test tester = new Regex_Test();
+		tester.checker("hackerrank");
+
+	}
+}
