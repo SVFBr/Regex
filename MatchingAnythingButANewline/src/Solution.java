42a41a6e95acb2dc3bf806dec7222f80b95b34d5
SVFBr <sheilafreire.br@gmail.com>
2016-09-13 16:33:15 -0300

    atualização...

--- MatchingAnythingButANewline/src/Solution.java
+++
@@ -1,9 +0,0 @@
-// https://www.hackerrank.com/challenges/matching-anything-but-new-line/submissions/code/27535038
-
-public class Solution {    
-
-    public static void main(String[] args) {
-        Tester tester = new Tester();
-		 tester.check("...\\....\\....\\....");
-    }
-    }
